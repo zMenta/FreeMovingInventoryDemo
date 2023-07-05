697aba8239a0b72439a0a4d80edb3ec3e32b4ad4
menta <brentano.l@gmail.com>
2023-07-05 16:21:02 -0300

    inventory code, autoload, etc

---
+++ autoloads/globals.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal on_item_focus(item: Item)
+signal on_container_focus(inventory: PanelContainer)
---
+++ inventory/inventory.gd
@@ -0,0 +1,5 @@
+extends MarginContainer
+
+var held_item : Item
+var item_with_focus : Item 
+var container_with_focus 
