4c3c47ba395f140f40bd06e42e588d7e8455db80
menta <brentano.l@gmail.com>
2023-07-09 14:14:34 -0300

    on item correct slot makes it VALID -> on process.

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -17,23 +17,25 @@ func _process(_delta: float) -> void:
 			return
 
 	if held_item != null:
-		if Input.is_action_just_pressed("mouse_left_click") and current_slot != null:
-			if current_slot.has_mouse_focus and current_slot.item_type.type == held_item.item_type.type:
-				if current_slot.stored_item == null:
-					current_slot.stored_item = held_item.duplicate()
-					current_slot.texture_rect.texture = held_item.texture_rect.texture
-					held_item.queue_free()
-					held_item = null
-				else:
-					var temporary_item: Item = held_item.duplicate()
-					current_slot.texture_rect.texture = held_item.texture_rect.texture
-					held_item.queue_free()
-					held_item = null
-					add_child(current_slot.stored_item)
-					_hold_item(current_slot.stored_item)
-					held_item.rotation = 0
-					current_slot.stored_item = temporary_item
-				return
+		if current_slot != null:
+			_check_slot_is_valid()
+			if Input.is_action_just_pressed("mouse_left_click"):
+				if current_slot.has_mouse_focus and current_slot.item_type.type == held_item.item_type.type:
+					if current_slot.stored_item == null:
+						current_slot.stored_item = held_item.duplicate()
+						current_slot.texture_rect.texture = held_item.texture_rect.texture
+						held_item.queue_free()
+						held_item = null
+					else:
+						var temporary_item: Item = held_item.duplicate()
+						current_slot.texture_rect.texture = held_item.texture_rect.texture
+						held_item.queue_free()
+						held_item = null
+						add_child(current_slot.stored_item)
+						_hold_item(current_slot.stored_item)
+						held_item.rotation = 0
+						current_slot.stored_item = temporary_item
+					return
 
 		if Input.is_action_just_pressed("mouse_left_click") and (held_item.state == Item.States.VALID or held_item.state == Item.States.FOCUS):
 			_place_item(held_item)
@@ -65,6 +67,11 @@ func _hold_item(item: Item) -> void:
 	held_item.is_selected = true
 	held_item.state = Item.States.VALID
 
+func _check_slot_is_valid() -> void:
+	if current_slot != null and held_item != null:
+		if current_slot.item_type.type == held_item.item_type.type and current_slot.has_mouse_focus:
+			held_item.state = Item.States.VALID
+
 
 func _on_item_focus(item: Item) -> void:
 	current_item = item
@@ -75,7 +82,7 @@ func _on_item_invalid_placement(item: Item) -> void:
 
 func _on_slot_mouse_entered(slot: Slot) -> void:
 	current_slot = slot
-	if current_slot != null and held_item != null:
-		if current_slot.item_type.type == held_item.item_type.type:
-			held_item.state = Item.States.VALID
+	_check_slot_is_valid()
+
+
 
